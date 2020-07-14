15e94d07741b150bd92775ccd145b0904e40c618
Rakshitkumar06 <62870599+Rakshitkumar06@users.noreply.github.com>
2020-07-14 14:12:49 +0530

    First commit

---
+++ controller/student.controller.js
@@ -0,0 +1,62 @@
+const StudentModel = require("../model/index").student;
+const assert  = require("assert");
+
+
+module.exports = {
+   allStudent: (req, res) => {
+       // read all students
+       StudentModel.find((err, response) => {
+           if(err) assert.equal(err, null);
+           res.json(response);
+       });
+   },
+   createStudent: (req, res) => {
+       // adding new student to db
+       let Student = new StudentModel(req.body);
+
+       student
+       .save()
+       .then((response) => {
+           res.status(200).json({ code: 200, message: "Student Created" });
+       })
+       .catch((err) => {
+           res.status(200).json({ code: 301, message: "unable to save student" });
+       });
+   },
+   showStudent : (req, res) => {
+       // viewing single student info
+       let id = req.params.id;
+       StudentModel.findById({ _id: id }, ( err, data) => {
+          if(err) assert.equal(err, null);
+          res.json(data);
+       });
+   },
+   updateStudent : (req, res) => {
+       let id = req.params.id;// read id from url address
+
+       let Student = new StudentModel(req.body);
+       StudentModel.findByIdAndUpdate({ _id: id }, student, (err, response) => {
+           if(err) {
+               assert.equal(err, null);
+            }else {
+               res
+               .status(200)
+               .json({ code: 200, message: "successfully updated student" });
+           }
+       });
+   },
+   deleteStudent : (req, res) => {
+       let id = req.params.id;
+
+       StudentModel.findByIdAndDelete({ _id: id}, (err, response) => {
+           if(err) {
+               assert.equal(null, err);
+               res.status(200).json({ code: 301, message: "Unable to delete" });
+           }else {
+               res
+               .status(200)
+               .json({ code: 200, message:"student deleted succesfully" });
+           }
+       });
+   },
+};
